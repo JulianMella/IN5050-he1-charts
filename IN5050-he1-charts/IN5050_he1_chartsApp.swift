247514cb468ba4465d53ba15bf93be986da26018
JulianMella <juliaame@uio.no>
2026-03-02 18:36:58 +0100

    Initial Commit

---
+++ IN5050-he1-charts/IN5050_he1_chartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IN5050_he1_chartsApp.swift
+//  IN5050-he1-charts
+//
+//  Created by Julian on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct IN5050_he1_chartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
